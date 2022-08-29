13489de9174d76367c9211302e1e29d5d7c91ca2
Bruno <ichigoplayer18@gmail.com>
2022-08-29 18:00:16 +0200

    Structure of project

---
+++ BackEnd/BudgetSpese/src/main/java/com/main/BudgetSpese/BudgetSpeseApplication.java
@@ -0,0 +1,13 @@
+package com.main.BudgetSpese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetSpeseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetSpeseApplication.class, args);
+	}
+
+}
---
+++ BackEnd/BudgetSpese/src/test/java/com/main/BudgetSpese/BudgetSpeseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.BudgetSpese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetSpeseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
